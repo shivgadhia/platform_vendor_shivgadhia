96dbddd024f98294811a5fc49c349d281c0f0ad3
Shivam Gadhia <shivam.gadhia@googlemail.com>
2015-01-18 12:52:34 +0000

    Update Android.mk

--- prebuilts/shubh/Android.mk
+++ prebuilts/shubh/Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 # Module name should match apk name to be installed.
 LOCAL_MODULE := Shubh.apk
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
    
